c80acccd2ee929484304716f92bd98912cc35865
Gustavo Fernandes <gustavosergio2001@gmail.com>
2025-04-24 11:00:07 -0300

    Sorteio Infinito

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ let departamentos = ["Recursos Humanos", "Financeiro", "Contabilidade", "Marketi
 let primario = ["Logística de Entrada", "Produção/Operação", "Logísitca de Saída", "Marketing/Vendas", "Serviços"];
 let suporte = ["Infraestrutura", "Recursos Humanos", "Tecnologia", "Aquisições"];
 
-function handleSorteio() {
-  for (let index = 0; index < 3; index++) {
-    console.log()
+let sorteioInterval;
 
-    switch (index) {
-      case 0:
-        document.querySelector('label.department').innerHTML = 'Departamento: ' + departamentos[Math.floor(Math.random() * departamentos.length)]
-        break;
-      case 1:
-        document.querySelector('label.primary').innerHTML =  'Atv. Primária: ' + primario[Math.floor(Math.random() * primario.length)]
-        break;
-      case 2:
-        document.querySelector('label.support').innerHTML =  'Atv. de Apoio: ' + suporte[Math.floor(Math.random() * suporte.length)]
-        break;
-    }
-  }
+function handleSorteio() {
+  document.querySelector('label.department').innerHTML = 'Departamento: ' + departamentos[Math.floor(Math.random() * departamentos.length)];
+  document.querySelector('label.primary').innerHTML = 'Atv. Primária: ' + primario[Math.floor(Math.random() * primario.length)];
+  document.querySelector('label.support').innerHTML = 'Atv. de Apoio: ' + suporte[Math.floor(Math.random() * suporte.length)];
+}
+
+function startSorteio() {
+  handleSorteio();
+  sorteioInterval = setInterval(handleSorteio, 1);
+}
+
+function stopSorteio() {
+  clearInterval(sorteioInterval);
 }
